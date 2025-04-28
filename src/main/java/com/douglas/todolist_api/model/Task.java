661b9147777cfdc082b984d44469a82f900819e7
Douglas Oliveira da Silva <douglas.silva@aguasdoimperador.com.br>
2025-04-28 16:44:33 -0300

    feat: criar entidade tarefa

---
+++ src/main/java/com/douglas/todolist_api/model/Task.java
@@ -0,0 +1,29 @@
+package com.douglas.todolist_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+* @Entity: diz ao Spring que essa classe vai virar uma tabela no banco.
+@Table(name = "tasks"): define o nome da tabela como "tasks".
+@Id + @GeneratedValue: define o campo id como chave primária e auto incremento.
+@Data, @AllArgsConstructor, @NoArgsConstructor: são do Lombok para gerar getters, setters, construtores automaticamente, deixando o código mais limpo.
+* */
+@Entity
+@Table (name = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    //Atributos
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+}
