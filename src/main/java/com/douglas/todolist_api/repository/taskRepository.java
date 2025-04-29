0522aeedf86edfb97c8ed35845031014fcd0c789
Douglas Oliveira da Silva <douglas.silva@aguasdoimperador.com.br>
2025-04-29 11:58:21 -0300

    feat: cria taskRepository com Spring Data JPA #2

---
+++ src/main/java/com/douglas/todolist_api/repository/taskRepository.java
@@ -0,0 +1,12 @@
+package com.douglas.todolist_api.repository;
+
+import com.douglas.todolist_api.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface taskRepository {
+    @Repository
+    public interface TaskRepository extends JpaRepository<Task, Long>{
+
+    }
+}
