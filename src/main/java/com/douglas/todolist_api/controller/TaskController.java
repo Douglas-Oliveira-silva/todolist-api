aa867f4a8f86eef17c3702877125073a8bed38b2
Douglas Oliveira da Silva <douglas.silva@aguasdoimperador.com.br>
2025-04-29 16:06:36 -0300

    feat:(controller): cria endpoints básicos do taskController #4

---
+++ src/main/java/com/douglas/todolist_api/controller/TaskController.java
@@ -0,0 +1,47 @@
+package com.douglas.todolist_api.controller;
+
+
+/*
+@RestController: Indica que essa classe é um controlador REST (responde com JSON, geralmente).
+@RequestMapping("/tasks"): Define o endpoint base da API. Todas as rotas aqui começam com /tasks.
+*/
+
+import com.douglas.todolist_api.model.Task;
+import com.douglas.todolist_api.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TaskController {
+
+        @Autowired
+        private TaskService taskService;
+
+        //GET / tasks
+        @GetMapping
+        public List<Task> getAllTasks(){
+         return taskService.findAllTasks();
+        }
+
+        //GET / tasks {id}
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id){
+            return taskService.findbyId(id);
+    }
+       // GET / Tasks
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+           return taskService.save(task);
+        }
+        // DELETE / Tasks/{id}
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+            taskService.deleteById(id);
+    }
+
+    }
+
+
+
